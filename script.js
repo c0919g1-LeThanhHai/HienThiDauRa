7123053b54aa3befa078f203ad56e12637c9cc1d
lethanhhai1998 <49983494+lethanhhai1998@users.noreply.github.com>
2019-09-25 12:00:17 +0700

    Hien thi dau ra

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    alert("Xin chào!");
+
+    document.getElementById("messageDiv").innerHTML="Xin chào 1!";
+
+    console.log("Xin chào 2!");
+}
+
+function showMessage2() {
+    document.write("Xin chào!");
+}
